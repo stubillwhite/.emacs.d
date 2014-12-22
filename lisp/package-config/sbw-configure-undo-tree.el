59736e35facb4e647d9e81fd4e573b6828f91e90
Stu White <stubillwhite@gmail.com>
2014-12-22 20:18:32 +0000

    Persist undo-tree state between sessions.

--- lisp/package-config/sbw-configure-undo-tree.el
+++ lisp/package-config/sbw-configure-undo-tree.el
@@ -6,6 +6,10 @@
     ;; Enable everywhere
     (global-undo-tree-mode t)
 
+    ;; Persistent undo between sessions, use the same directory as other backups
+    (setq undo-tree-auto-save-history t)
+    (setq undo-tree-history-directory-alist backup-directory-alist)
+
     ;; Easy alias
     (defalias 'redo 'undo-tree-redo)))
 
