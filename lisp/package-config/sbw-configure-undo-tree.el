bf7139dd038fa0898951632b6e6ac95754a76b56
Stu White <stubillwhite@gmail.com>
2014-12-23 21:35:50 +0000

    Fixed undo-tree session data backup directory.

--- lisp/package-config/sbw-configure-undo-tree.el
+++ lisp/package-config/sbw-configure-undo-tree.el
@@ -7,9 +7,10 @@
     (global-undo-tree-mode t)
 
     ;; Persistent undo between sessions, use the same directory as other backups
-    (setq undo-tree-auto-save-history t)
-    (setq undo-tree-history-directory-alist backup-directory-alist)
-
+    (setq
+      undo-tree-auto-save-history       t
+      undo-tree-history-directory-alist (quote (("." . "~/.emacs-undo-tree/"))))
+    
     ;; Easy alias
     (defalias 'redo 'undo-tree-redo)))
 
