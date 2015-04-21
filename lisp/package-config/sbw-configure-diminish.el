fb0c07580c959051fc9a922e02f7304be3868b26
Stu White <stubillwhite@gmail.com>
2015-04-21 22:23:18 +0100

    Diminish magit mode.

--- lisp/package-config/sbw-configure-diminish.el
+++ lisp/package-config/sbw-configure-diminish.el
@@ -3,8 +3,8 @@
 (use-package diminish
   :config
   (progn
-    (eval-after-load
-      "Org-Indent"
-      '(diminish 'org-indent-mode))))
+    (eval-after-load "Org-Indent" '(diminish 'org-indent-mode))
+    (eval-after-load "Magit"      '(diminish 'magit-auto-revert-mode))
+    ))
 
 (provide 'sbw-configure-diminish)
