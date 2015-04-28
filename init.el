ef02b3c0a0c7d2fcb58884de05dd9aeed4c46373
Stu White <stubillwhite@gmail.com>
2015-04-28 23:21:16 +0100

    Tweaks to new bootstrap mechanism.

--- init.el
+++ init.el
@@ -13,6 +13,9 @@
 
 (add-hook 'after-init-hook 'sbw/init-report-time-elapsed 'append)
 
+(defun sbw/init-message (s)
+  (message (concat "\n" s)))
+
 ;; Add Lisp package locations
 (setq sbw/lisp-path "~/.emacs.d/lisp")
 (add-to-list 'load-path sbw/lisp-path)
@@ -22,11 +25,28 @@
 
 (if sbw/use-new-configuration
   (progn
-    ;; Require the core scripts and bootstrap the system
+    ;; Bootstrap the system and install all packages
     (require 'sbw-common-config)
-    (require 'sbw-new-bootstrap)
+    (require 'new-bootstrap)
 
-    )
+    (sbw/init-message "Initializing bootstrap")
+    (sbw/new-bootstrap-initialize)
+
+    (sbw/init-message "Installing packages")
+    (sbw/new-bootstrap-install sbw/new-bootstrap-all-packages)
+
+    (sbw/init-message "Requiring core packages")
+    (sbw/new-bootstrap-require sbw/new-bootstrap-core-packages)
+    
+    (sbw/init-message "Loading package configuration")
+    (sbw/new-bootstrap-configure "~/.emacs.d/lisp/package-config" sbw/new-bootstrap-all-packages)
+
+    (sbw/init-message "Loading personal packages")
+    (sbw/new-bootstrap-require sbw/new-bootstrap-personal-packages)
+
+    (sbw/init-message "Loading tests")
+    (sbw/new-bootstrap-load-elisp-files "~/.emacs.d/lisp/test"))
+  
   (progn
     ;; Require the core scripts and bootstrap the system
     (require 'sbw-common-config)
@@ -46,3 +66,4 @@
 
     ;; Finally, load all tests
     (sbw/bootstrap-load-elisp-files "~/.emacs.d/lisp/test")))
+
--- lisp/new-bootstrap.el
+++ lisp/new-bootstrap.el
@@ -1,80 +1,96 @@
 ;; Package bootstrapping
 
-(require 'package)
-
 (defconst sbw/new-bootstrap-core-packages
-  '( (:name "use-package")     ;; Easy package use
-     (:name "dash")            ;; Modern list library
-     (:name "dash-functional") ;; Further functions for dash
-     (:name "f")               ;; Modern file API
-     (:name "s")               ;; Modern string API
-     (:name "names")           ;; Sane namespace handling
-     (:name "seq")             ;; Unified sequence handling
+  '( (:name use-package)     ;; Easy package use
+     (:name dash)            ;; Modern list library
+     (:name dash-functional) ;; Further functions for dash
+     (:name f)               ;; Modern file API
+     (:name s)               ;; Modern string API
+     (:name names)           ;; Sane namespace handling
+     (:name seq)             ;; Unified sequence handling
      )
   "List of the core packages required by everything else, which have no dependencies.")
 
 (defconst sbw/new-bootstrap-additional-packages
-  '( (:name "color-theme") ;; Color theme support
-     (:name "undo-tree")   ;; Undo tree visualisation
-     (:name "flyspell")    ;; Spell checking
+  '( (:name color-theme) ;; Color theme support
+     (:name undo-tree)   ;; Undo tree visualisation
+     (:name flyspell)    ;; Spell checking
 
      ;; org-mode
-     (:name "org")     ;; Your life in plain text
-     (:name "htmlize") ;; html org-mode reports
+     (:name org)     ;; Your life in plain text
+     (:name htmlize) ;; html org-mode reports
 
      ;; Git
-     (:name "magit") ;; Control Git from Emacs
+     (:name magit) ;; Control Git from Emacs
 
      ;; Auto-complete
-     (:name "company") ;; Auto-completion
+     (:name company) ;; Auto-completion
 
      ;; Interface
      ;;smart-mode-line ;; Better mode line
      ;;smart-mode-line-powerline-theme ;; Powerline theme for smart-mode-line
-     (:name "projectile")      ;; Project interaction library for Emacs
-     (:name "ace-jump-mode")   ;; Faster movement
-     (:name "switch-window")   ;; Faster switching between windows
-     (:name "expand-region")   ;; Expand region by semantic units
-     (:name "hydra")           ;; Make Emacs bindings that stick around
-     (:name "worf")            ;; Vi-like bindings for org-mode
+     (:name projectile)      ;; Project interaction library for Emacs
+     (:name ace-jump-mode)   ;; Faster movement
+     (:name switch-window)   ;; Faster switching between windows
+     (:name expand-region)   ;; Expand region by semantic units
+     (:name hydra)           ;; Make Emacs bindings that stick around
+     ;;(:name worf)            ;; Vi-like bindings for org-mode
 
      ;;nyan-mode       ;; Nyan-nyan-nyan-nyan-nyan nyan nyan nyan
-     (:name "powerline")
-     (:name "diminish")
+     (:name powerline)
+     (:name diminish)
      
      ;; Helm
-     (:name "helm")            ;; Incremental narrowing framework
-     (:name "helm-swoop")      ;; Efficiently skipping between matches
-     (:name "helm-projectile") ;; Helm integration for projectile
+     (:name helm)            ;; Incremental narrowing framework
+     (:name helm-swoop)      ;; Efficiently skipping between matches
+     (:name helm-projectile) ;; Helm integration for projectile
      
      ;; Clojure
-     (:name "clojure-mode")      ;; Clojure mode
-     (:name "cider")             ;; REPL support
-     (:name "smartparens")       ;; Improved paredit
-     (:name "ac-cider")          ;; Cider REPL autocomplete and documentation
-     (:name "expectations-mode") ;; Clojure expectations minor mode
+     (:name clojure-mode)      ;; Clojure mode
+     (:name cider)             ;; REPL support
+     (:name smartparens)       ;; Improved paredit
+     (:name ac-cider)          ;; Cider REPL autocomplete and documentation
+     (:name expectations-mode) ;; Clojure expectations minor mode
 
      ;; Graphviz
-     (:name "graphviz-dot-mode") ;; Graphviz DOT file support and previews
+     (:name graphviz-dot-mode) ;; Graphviz DOT file support and previews
 
      ;; Markdown
-     (:name "markdown-mode") ;; Markdown mode
+     (:name markdown-mode) ;; Markdown mode
 
      ;; Groovy
-     (:name "groovy-mode") ;; Groovy mode
+     (:name groovy-mode) ;; Groovy mode
 
      ;; Yasnippets
-     (:name "yasnippet") ;; Yet another snippet extension for Emacs
+     (:name yasnippet) ;; Yet another snippet extension for Emacs
 
      ;; Elisp
-     (:name "json")  ;; JavaScript Object Notation parser / generator
-     (:name "async") ;; Asynchronous processing in Emacs
+     (:name json)  ;; JavaScript Object Notation parser / generator
+     (:name async) ;; Asynchronous processing in Emacs
 
      ;; XML
      ;;nxml-mode ;; Major mode for editing XML
      ;;el-get
      )
   "List of the additional packages required for this Emacs configuration.")
+
+(defconst sbw/new-bootstrap-all-packages
+  (append sbw/new-bootstrap-core-packages sbw/new-bootstrap-additional-packages)
+  "List of all the packages required for this Emacs configuration.")
+
+(defconst sbw/new-bootstrap-personal-packages
+  '( (:name sbw-bindings)
+     (:name sbw-cosmetics)
+     (:name sbw-cosmetics-code-style)
+     (:name sbw-countdown)
+     (:name sbw-hash-tables)
+     (:name sbw-menu)
+     (:name sbw-misc)
+     (:name sbw-multimethods)
+     (:name sbw-org-review)
+     (:name sbw-time)
+     (:name sbw-utils) )
+  "List of my packages.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -101,20 +117,47 @@
   (add-to-list 'el-get-recipe-path "~/.emacs.d/lisp/recipes"))
 
 (defun sbw/new-bootstrap-initialize ()
+  "Initialise the bootstrap mechanism."
   (sbw/new-bootstrap--install-el-get)
   (sbw/new-bootstrap--configure-recipes))
 
-(defun sbw/new-bootstrap--install-package (config)
-  (sbw/new-bootstrap--with-package-config config
-    (message (concat "Installing " name))
-    (eval `(el-get-bundle ,name))))
-
 (defun sbw/new-bootstrap-install (pkg-list)
-  (mapc 'sbw/new-bootstrap--install-package pkg-list))
+  "Install the specified packages."
+  (switch-to-buffer "*Messages*")
+  (mapc
+    (lambda (x)
+      (sbw/new-bootstrap--with-package-config x
+        (message (concat "Installing " (symbol-name name)))
+        (eval `(el-get-bundle (symbol-name ,name)))))
+    pkg-list))
 
-(sbw/new-bootstrap-initialize)
-(sbw/new-bootstrap-install sbw/new-bootstrap-core-packages)
-(sbw/new-bootstrap-install sbw/new-bootstrap-additional-packages)
+(defun sbw/new-bootstrap-require (pkg-list)
+  "Require the specified packages."
+  (mapc
+    (lambda (x)
+      (sbw/new-bootstrap--with-package-config x
+        (message (concat "Requiring " (symbol-name name)))
+        (eval `(require (quote ,name)))
+        ))
+    pkg-list))
+
+(defun sbw/new-bootstrap-configure (dir pkg-list)
+  "Configure the specified packages with the configuration files in DIR."
+  (mapc
+    (lambda (x)
+      (sbw/new-bootstrap--with-package-config x
+        (let* ( (pkg  (symbol-name name))
+                (fnam (concat dir "/sbw-configure-" pkg ".el")) )
+          (if (f-file? fnam)
+            (load-file fnam)
+            (eval `(use-package ,pkg))))))
+    pkg-list))
+
+(defun sbw/bootstrap-load-elisp-files (dir)
+  "Load the Elisp files in DIR."
+  (mapc
+    (lambda (x) (load-file x))
+    (directory-files dir :full-name ".*\.el")))
 
 
-(provide 'sbw-new-bootstrap)
+(provide 'new-bootstrap)
