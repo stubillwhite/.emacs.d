f60115d5117fbce5508eeff0a488637d1e578859
Stu White <stubillwhite@gmail.com>
2015-05-25 07:56:34 +0100

    Minor housekeeping.

--- lisp/package-config/sbw-configure-flyspell.el
+++ lisp/package-config/sbw-configure-flyspell.el
@@ -9,8 +9,8 @@
     (require 'ispell)
     
     ;; Spell check all text modes
-    (dolist (hook '(text-mode-hook))
-      (add-hook hook (lambda () (flyspell-mode 1))))
+    (dolist (x '(text-mode-hook))
+      (add-hook x (lambda () (flyspell-mode 1))))
 
     ;; Aspell is slow, so use ultra mode to improve performance
     (add-to-list 'ispell-extra-args "--sug-mode=ultra")
--- lisp/sbw-multimethods.el
+++ lisp/sbw-multimethods.el
@@ -38,105 +38,21 @@
   (let ( (f-symbol (intern (symbol-name name))) )
     `(sbw/mm--register-dispatch-handler (quote ,f-symbol) ,dispatch-val (lambda ,sig ,body))))
 
-
-
-
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun parity (x &rest args)
-  (if (= 0 (mod x 2)) :even :odd))
-
-(setq *sbw/mm--registry* (sbw/ht-create))
-
-(sbw/mm-defmulti describe-number 'parity)
-(sbw/mm-defmethod describe-number [:even] (x y) (print (format "The number %d is even" x)))
-(sbw/mm-defmethod describe-number [:odd]  (x y) (print (format "The number %d is odd" x)))
-
-(describe-number 1 2)
-(describe-number 2 2)
-
-(print *sbw/mm--registry*)
-
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Type-Predicates.html#Type-Predicates
-(defun sbw/data-type (x &rest args)
-  (cond
-    ((stringp x)      :string)
-    ((keywordp x)     :keyword)
-    ((symbolp x)      :symbol)
-    ((numberp x)      :number)
-    ((hash-table-p x) :hash-table)
-    ((listp x)        :list)
-    ((vectorp x)      :vector)
-    (t                (signal 'wrong-type-argument (list x)))))
-
-
-(defun sbw/pprint (x)
-  (sbw/-pprint-to-string x 0))
-
-(defun sbw/-pprint-indent (s depth)
-  (concat (s-repeat depth " ") s "\n"))
-
-(defun sbw/-pprint-string-to-string (x depth)
-  (sbw/-pprint-indent x depth))
-
-(defun sbw/-pprint-keyword-to-string (x depth)
-  (sbw/-pprint-indent (symbol-name x) depth))
-
-(defun sbw/-pprint-number-to-string (x depth)
-  (sbw/-pprint-indent (number-to-string x) depth))
-
-(defun sbw/-pprint-vector-to-string (x depth)
-  (concat
-    (sbw/-pprint-indent "[" depth)
-    (apply 'concat
-      (-map (lambda (x) (sbw/-pprint-to-string x (+ 4 depth))) x))
-    (sbw/-pprint-indent "]" depth)))
-
-(defun sbw/-pprint-hash-table-to-string (x depth)
-  (concat
-    (sbw/-pprint-indent "{" depth)
-    (-reduce-from
-      (lambda (acc k) (concat
-                   acc
-                   (sbw/-pprint-to-string k (+ 4 depth))
-                   ":"
-                   (sbw/-pprint-to-string (sbw/ht-get x k) (+ 4 depth))))
-      ""
-      (sbw/ht-keys x))
-    (sbw/-pprint-indent "}" depth)))
-
-(sbw/mm-defmulti sbw/-pprint-to-string 'sbw/data-type)
-(sbw/mm-defmethod sbw/-pprint-to-string [:string]     (x depth) (sbw/-pprint-string-to-string x depth))
-(sbw/mm-defmethod sbw/-pprint-to-string [:keyword]    (x depth) (sbw/-pprint-keyword-to-string x depth))
-(sbw/mm-defmethod sbw/-pprint-to-string [:number]     (x depth) (sbw/-pprint-number-to-string x depth))
-(sbw/mm-defmethod sbw/-pprint-to-string [:hash-table] (x depth) (sbw/-pprint-hash-table-to-string x depth))
-;(sbw/mm-defmethod sbw/-pprint-to-string [:list]       (x depth) (sbw/-pprint-number-to-string x depth))
-(sbw/mm-defmethod sbw/-pprint-to-string [:vector]     (x depth) (sbw/-pprint-vector-to-string x depth))
-
-
-(defun sbw/pprint (x)
-  "Return X as a pretty-printed string."
-  (let ( (json-encoding-pretty-print t) )
-    (s-replace "\"" "" (json-encode x))))
-
-(sbw/pprint (sbw/ht-create "foo" 23))
-
-
-
-(sbw/pprint "foo")
-(sbw/pprint :foo)
-(sbw/pprint 23)
-(sbw/pprint (sbw/ht-create :k1 :v1 :k2 (sbw/ht-create :k3 :v3)))
-;(sbw/pprint (list 1 2 (list 3 4)))
-(sbw/pprint [1 2 [3 4]])
-
-
-(defun pprint (form &optional output-stream)
-  (princ (with-temp-buffer
-           (cl-prettyprint form)
-           (buffer-string))
-    output-stream))
+;; 
+;; (defun parity (x &rest args)
+;;   (if (= 0 (mod x 2)) :even :odd))
+;; 
+;; (setq *sbw/mm--registry* (sbw/ht-create))
+;; 
+;; (sbw/mm-defmulti describe-number 'parity)
+;; (sbw/mm-defmethod describe-number [:even] (x y) (print (format "The number %d is even" x)))
+;; (sbw/mm-defmethod describe-number [:odd]  (x y) (print (format "The number %d is odd" x)))
+;; 
+;; (describe-number 1 2)
+;; (describe-number 2 2)
+;; 
+;; (print *sbw/mm--registry*)
+;; 
 
 (provide 'sbw-multimethods)
