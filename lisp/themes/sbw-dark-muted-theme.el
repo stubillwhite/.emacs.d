7e2f403021e0ea02bdaeeece5d14649a78d1faac
Stu White <stubillwhite@gmail.com>
2015-06-01 17:28:20 +0100

    Updated theme.

--- lisp/themes/sbw-dark-muted-theme.el
+++ lisp/themes/sbw-dark-muted-theme.el
@@ -7,12 +7,9 @@
 (defun sbw/theme--create-face (name spec)
   (eval `(defface ,name ,spec "Face ,name." :group 'sbw-dark-muted-faces)))
 
-(defun sbw/theme--inherit-from-face (name base-name)
-  `(,name ((t (:inherit ,base-name)))))
-
 (let* ( (*normal-bg*     "gray10")
         (*normal-fg*     "gray80")
-        (*emphasis-bg*   "darkslategray")
+        (*emphasis-bg*   "gray10")
         (*emphasis-fg*   "gray100")
 
         (*selection-bg*  "gray30")
@@ -48,36 +45,44 @@
        (sbw-dark-muted-constant            `((t (:foreground ,*item-2*      :background ,*normal-bg*))))
        (sbw-dark-muted-mismatch            `((t (:foreground ,*mismatch-fg* :background ,*mismatch-bg*))))
        
-
-
-       
        ))
-
-  
-
-  
-
-
   
   (custom-theme-set-faces
     'sbw-dark-muted
 
     `(default                ((t (:background ,*normal-bg* :foreground ,*normal-fg*))))
     
-    `(bold                   ((t (:inherit sbw-dark-muted-normal   :bold t))))
-    `(underline              ((t (:inherit sbw-dark-muted-normal   :underline t))))
-    `(highlight              ((t (:inherit sbw-dark-muted-emphasis))))
-    `(lazy-highlight         ((t (:inherit sbw-dark-muted-emphasis))))
+    '(bold                   ((t (:inherit sbw-dark-muted-normal   :bold t))))
+    '(underline              ((t (:inherit sbw-dark-muted-normal   :underline t))))
+    '(highlight              ((t (:inherit sbw-dark-muted-match))))
+    '(lazy-highlight         ((t (:inherit sbw-dark-muted-match))))
 
-    `(isearch                ((t (:inherit sbw-dark-muted-match))))
-    `(isearch-fail           ((t (:inherit sbw-dark-muted-mismatch))))
+    '(isearch                ((t (:inherit sbw-dark-muted-match))))
+    '(isearch-fail           ((t (:inherit sbw-dark-muted-mismatch))))
 
-    `(show-paren-match       ((t (:inherit sbw-dark-muted-match))))
-    `(show-paren-mismatch    ((t (:inherit sbw-dark-muted-mismatch))))
+    '(show-paren-match       ((t (:inherit sbw-dark-muted-match))))
+    '(show-paren-mismatch    ((t (:inherit sbw-dark-muted-mismatch))))
 
-    `(font-lock-comment-face           ((t (:inherit sbw-dark-muted-comment))))
-    `(font-lock-comment-delimiter-face ((t (:inherit sbw-dark-muted-comment))))
-    `(font-lock-string-face            ((t (:inherit sbw-dark-muted-string))))
+    '(font-lock-comment-delimiter-face ((t (:inherit sbw-dark-muted-comment))))
+    '(font-lock-comment-face           ((t (:inherit sbw-dark-muted-comment))))
+    '(font-lock-constant-face          ((t (:inherit sbw-dark-muted-constant))))
+    '(font-lock-string-face            ((t (:inherit sbw-dark-muted-string))))
+    '(font-lock-type-face              ((t (:inherit sbw-dark-muted-normal))))
+
+    '(org-agenda-clocking    ((t (:inherit sbw-dark-muted-normal))))
+    '(org-agenda-done        ((t (:inherit sbw-dark-muted-normal))))
+    '(org-checkbox           ((t (:inherit sbw-dark-muted-emphasis))))
+    '(org-done               ((t (:inherit sbw-dark-muted-emphasis))))
+    '(org-level-1            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-2            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-3            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-4            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-5            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-6            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-7            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-level-8            ((t (:inherit sbw-dark-muted-normal))))
+    '(org-priority           ((t (:inherit sbw-dark-muted-normal))))
+    '(org-todo               ((t (:inherit sbw-dark-muted-emphasis))))
     ))
 
  
