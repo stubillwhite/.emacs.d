53c86e737406f09c92ce382feefc669fc66c9fd0
Stu White <stubillwhite@gmail.com>
2015-06-15 19:57:42 +0100

    Added Golden Ratio to window mode.

--- lisp/package-config/sbw-configure-hydra.el
+++ lisp/package-config/sbw-configure-hydra.el
@@ -11,24 +11,25 @@
     (defhydra sbw/hydra-window (:color amaranth :hint nil)
       "
 ^Navigate^       ^Modify^
-^-------------------------------------
+^--------------------------------------
 _h_: Left        _V_: Vertical split
 _j_: Down        _H_: Horizontal split
 _k_: Up          _s_: Swap
 _l_: Right       _d_: Delete
-_a_: Ace-Window
-_q_: Exit
+_a_: Ace-Window  _g_: Golden ratio mode
+_q_: Quit
 "
       ("h" windmove-left)
       ("j" windmove-down)
       ("k" windmove-up)
       ("l" windmove-right)
       ("a" ace-window)
+      ("g" golden-ratio-mode)
       ("V" split-window-horizontally)
       ("H" split-window-vertically)
       ("s" (lambda () (interactive) (ace-window 4)))
-      ("d" (lambda () (interactive) (ace-window 16)))
-      ("q" (lambda () (interactive) nil) :color blue)))
+      ("d" delete-window)
+      ("q" nil :color blue)))
   
   :bind
   ("C-c w" . sbw/hydra-window/body))
