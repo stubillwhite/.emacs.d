13427f43ddf1204d2e78e76e6dc5d0b0e3e96f42
Stu White <stubillwhite@gmail.com>
2015-08-06 19:17:31 +0100

    Remove spurious customise settings

--- init.el
+++ init.el
@@ -49,18 +49,4 @@
 
 (sbw/init-message "Unused configurations")
 (sbw/bootstrap-display-unused-configurations "~/.emacs.d/lisp/package-config" sbw/pkg-all-packages)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-  '(custom-safe-themes
-     (quote
-       ("fc3930cf24b9cfbdd32d47dc552f340c20f1b9040fb2aaa39758da1719b23ac7" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-done ((t (:weight normal :strike-through t))))
- '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:strike-through t)))))
+
