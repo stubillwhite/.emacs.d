b9646723665362e7b956164d50a89717a36763d3
Stu White <stubillwhite@gmail.com>
2015-09-07 16:48:20 +0100

    Correct dictionary location on Cygwin

--- lisp/package-config/sbw-configure-flyspell.el
+++ lisp/package-config/sbw-configure-flyspell.el
@@ -18,10 +18,25 @@
     (add-to-list 'ispell-extra-args "--sug-mode=ultra")
     
     (setq
-      ispell-silently-savep      t           ;; Don't prompt when adding to the dictionary
-      ispell-personal-dictionary "~/.ispell" ;; Personal dictionary location
-      ispell-dictionary          "british"   ;; British English
-      ))
+      ispell-silently-savep      t         ;; Don't prompt when adding to the dictionary
+      ispell-dictionary          "british" ;; British English
+      )
+
+    ;; Personal dictionary location        
+    (setq ispell-personal-dictionary
+      (cond
+        ((eq system-type 'cygwin)    "/cygwin64/home/GB105549/.emacs.d/.ispell")
+        ((eq system-type 'gnu/linux) "~/.emacs.d/.ispell"))))
+
+  
+
+  (defun sbw/ispell-configure-org-mode-ignored-regions ()
+    (make-local-variable 'ispell-skip-region-alist)
+    (add-to-list 'ispell-skip-region-alist '(org-property-drawer-re))
+    (add-to-list 'ispell-skip-region-alist '("^#\\+begin_src" . "^#\\+end_src")))
+  ;; (add-hook 'org-mode-hook 'sbw/ispell-configure-org-mode-ignored-regions)
+
+  
 
   :bind
   ("C-c s w" . ispell-word)
