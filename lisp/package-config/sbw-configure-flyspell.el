8ab2903a11bb7a7371337aba2d235b6519c38fa7
stubillwhite <stubillwhite@gmail.com>
2015-10-09 23:22:58 +0100

    Separate Windows and Linux dictionaries

--- lisp/package-config/sbw-configure-flyspell.el
+++ lisp/package-config/sbw-configure-flyspell.el
@@ -25,8 +25,8 @@
     ;; Personal dictionary location        
     (setq ispell-personal-dictionary
       (cond
-        ((sbw/is-windows?) "/cygwin64/home/GB105549/.emacs.d/.ispell")
-        ((sbw/is-linux?)   "~/.emacs.d/.ispell"))))
+        ((sbw/is-windows?) "/cygwin64/home/GB105549/.emacs.d/.ispell-win")
+        ((sbw/is-linux?)   "~/.emacs.d/.ispell-unix"))))
 
   
 
