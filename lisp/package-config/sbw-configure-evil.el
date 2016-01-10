d82715c54a3bf65b1a0f66ee528962f6a9dec99d
stubillwhite <stubillwhite@gmail.com>
2016-01-10 16:27:15 +0000

    Remove dead code

--- lisp/package-config/sbw-configure-evil.el
+++ lisp/package-config/sbw-configure-evil.el
@@ -19,23 +19,9 @@
           (setq sbw/evil--good-cursor-type cursor-type)
           (evil-mode))))
 
-    ;; (defun sbw/evil-escape (prompt)
-    ;;   (cond
-    ;;    ((or (evil-insert-state-p) (evil-normal-state-p) (evil-replace-state-p) (evil-visual-state-p)) [escape])
-    ;;    (t (kbd "C-g"))))
-
-    ;; (defun sbw/evil--configure-escape-alternative-key ()
-    ;;   (define-key key-translation-map (kbd "C-l") 'sbw/evil-escape)
-    ;;   (define-key evil-operator-state-map (kbd "C-l") 'keyboard-quit)
-    ;;   (set-quit-char "C-l"))
-
-    ;; (add-hook 'evil-mode-hook 'sbw/evil--configure-escape-alternative-key)
-
     (require 'key-chord)
-    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-    )
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state))
 
   :bind
   ("C-c v" . sbw/evil-toggle-global-evil-mode)
-  ("M-["   . sbw/evil-toggle-global-evil-mode)
-  )
+  ("M-["   . sbw/evil-toggle-global-evil-mode))
