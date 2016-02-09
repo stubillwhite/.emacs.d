a07ec94f33eccb40a685e6b161c4929a7f12d762
stubillwhite <stubillwhite@gmail.com>
2016-02-09 21:35:44 +0000

    Disable drag-stuff-mode in org-mode

--- lisp/package-config/sbw-configure-drag-stuff.el
+++ lisp/package-config/sbw-configure-drag-stuff.el
@@ -1,16 +1,22 @@
 (require 'use-package)
 
+(drag-stuff-mode 0)
+
 (use-package drag-stuff
   :diminish drag-stuff-mode
 
   :init
   (progn
+    ;; Enable in modes where it is useful...
     (let ( (hooks '(clojure-mode-hook
                     emacs-lisp-mode-hook
                     groovy-mode-hook
                     text-mode-hook)) )
       (dolist (hook hooks)
-        (add-hook hook 'drag-stuff-mode))))
+        (add-hook hook 'drag-stuff-mode)))
+
+    ;; ... but ensure that org-mode is not affected
+    (add-hook 'org-mode-hook (lambda () (drag-stuff-mode 0))))
 
   :config
   (progn))
