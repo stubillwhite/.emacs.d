bc5ff963ec360af19bfb76c1f6b62c02c5e40ae6
Stuart White <s.white.1@elsevier.com>
2016-10-31 06:44:44 +0000

    Elm REPL window config

--- lisp/package-config/sbw-configure-elm-mode.el
+++ lisp/package-config/sbw-configure-elm-mode.el
@@ -14,18 +14,23 @@
     ;; Autocomplete via Company
     (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
     (add-to-list 'company-backends 'company-elm)
-    
-    (defun sbw/elm-mode--send-current-buffer-to-repl ()
-      (interactive)
-      (let* ((curr (current-buffer)))
+
+    ;; Vertical split when splitting to the REPL
+    (defun sbw/elm-mode--ensure-vertical-split (orig-fun &rest args)
+      (let* ( (orig-width  split-width-threshold)
+              (orig-height split-height-threshold) )
         (save-some-buffers t)
-        (sbw/open-and-switch-to-window (get-buffer "*elm*"))
-        (switch-to-buffer-other-window curr)
-        (elm-repl-load)
-        )))
+        (setq split-width-threshold  0
+              split-height-threshold nil)    
+        (apply orig-fun args)
+        (setq split-width-threshold  orig-width
+              split-height-threshold orig-height)))
+
+    (advice-add 'elm-repl-load :around #'sbw/elm-mode--ensure-vertical-split))
   
   :bind
   (:map elm-mode-map
-        ("<f5>" . sbw/elm-mode--send-current-buffer-to-repl)))
+        ("<f5>" . elm-repl-load)))
 
 (provide 'sbw-configure-elm-mode)
+
