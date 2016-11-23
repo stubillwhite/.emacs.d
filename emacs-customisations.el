d7d3dd7454bcbe25fd8e6b1cae1406fee3877a7d
Stuart White <s.white.1@elsevier.com>
2016-11-23 22:02:30 +0000

    Segregate built-in Emacs customisations

---
+++ emacs-customisations.el
@@ -0,0 +1,16 @@
+;; File to store Emacs customisations to keep them out of my files
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (nil speed-type rainbow-mode queue org magit inflections groovy-mode flycheck evil dash-functional company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
