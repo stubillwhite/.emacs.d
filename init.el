3fd4952fafb3da7e976bcbdd21295b283ee50e99
Stuart White <s.white.1@elsevier.com>
2017-08-19 14:39:05 +0100

    Prevent package.el running at startup

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 ;; Maximum message history, track how long initialisation takes
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (setq message-log-max 16384)
 (setq-default use-package-verbose t)
 
