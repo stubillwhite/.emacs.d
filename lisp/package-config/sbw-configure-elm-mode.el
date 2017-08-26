4aa0032abf27e89cab6ac8fabc6aa1f8c7654062
Stuart White <s.white.1@elsevier.com>
2017-08-26 09:01:03 +0100

    Ensure elm-mode tests can be run from anywhere in the project

--- lisp/package-config/sbw-configure-elm-mode.el
+++ lisp/package-config/sbw-configure-elm-mode.el
@@ -25,16 +25,27 @@
         (apply orig-fun args)
         (setq split-width-threshold  orig-width
               split-height-threshold orig-height)))
-
     (advice-add 'elm-repl-load :around #'sbw/elm-mode--ensure-vertical-split)
 
+    ;; Colorize elm-test output
     (defun sbw/elm-mode--colorize-elm-test-buffer ()
       (when (eq major-mode 'compilation-mode)
         (sbw/display-ansi-colors)))
-    (add-hook 'compilation-filter-hook 'sbw/elm-mode--colorize-elm-test-buffer))
-  
+    (add-hook 'compilation-filter-hook 'sbw/elm-mode--colorize-elm-test-buffer)
+
+    ;; Ensure that tests can be run from src/ or tests/ directory
+    (defun sbw/elm-mode--ignore-tests-dependency-file (orig-fun &rest args)
+      (if-let ((result (apply orig-fun args)))
+          (if (s-suffix? "tests/" result)
+              (let ((default-directory (concat result "..")))
+                (elm--find-dependency-file-path))
+            result)))
+    (advice-add 'elm--find-dependency-file-path :around #'sbw/elm-mode--ignore-tests-dependency-file))
+
   :bind
   (:map elm-mode-map
         ("<f5>" . elm-repl-load)))
 
 (provide 'sbw-configure-elm-mode)
+
+
