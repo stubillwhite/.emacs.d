4b83da08ee50aff7bdfc3c4e457f79d30ff137f1
Stuart White <s.white.1@elsevier.com>
2017-10-15 10:16:10 +0100

    Update bindings for Slack mode

--- lisp/package-config/sbw-configure-slack.el
+++ lisp/package-config/sbw-configure-slack.el
@@ -42,26 +42,47 @@
      :subscribed-channels '())
 
     (slack-register-team
-     :name                "bos"
+     :name                "Elsevier"
      :client-id           sbw/slack-bos-client-id
      :client-secret       sbw/slack-bos-client-secret
      :token               sbw/slack-bos-token
      :subscribed-channels '())
+
+    ;; (slack-register-team
+    ;;  :name                "Elm"
+    ;;  :client-id           sbw/slack-elm-client-id
+    ;;  :client-secret       sbw/slack-elm-client-secret
+    ;;  :token               sbw/slack-elm-token
+    ;;  :subscribed-channels '())
+
+    ;; (slack-register-team
+    ;;  :name                "Clojurians"
+    ;;  :client-id           sbw/slack-clojurians-client-id
+    ;;  :client-secret       sbw/slack-clojurians-client-secret
+    ;;  :token               sbw/slack-clojurians-token
+    ;;  :subscribed-channels '()
+    ;;  )
 
     ;; General configuration
 
     ;; Suppress user statuses
     (defun slack-user-status (_id _team) "")
 
-    )
+    ;; (defun sbw/slack--force-face-height (orig-fun &rest args)
+    ;;   (message "Forcing to 12")
+    ;;   120)
+    ;; (advice-add 'emojify-default-font-height :around #'sbw/slack--force-font-height)
 
-  (setq slack-prefer-current-team      t
-        slack-display-team-name        nil
-        slack-buffer-function          #'switch-to-buffer)
-  
-  (define-key slack-mode-map "@"
+    (setq slack-prefer-current-team      t
+          slack-display-team-name        nil
+          slack-buffer-function          #'switch-to-buffer)
+    
     (defun sbw/slack-message-embed-mention ()
       (interactive)
-      (call-interactively #'slack-message-embed-mention))))
+      (call-interactively #'slack-message-embed-mention)))
+  
+  :bind
+  (:map slack-mode-map
+        ("@" . sbw/slack-message-embed-mention)))
 
 (provide 'sbw-configure-slack)
