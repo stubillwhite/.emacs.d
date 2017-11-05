58cb9cfe8c2c1dbe40cb08f728c2a0db89a44b7f
Stuart White <s.white.1@elsevier.com>
2017-11-05 07:35:39 +0000

    Update sprint end date

--- lisp/sbw-org-review.el
+++ lisp/sbw-org-review.el
@@ -275,7 +275,7 @@
      (sbw/org-review--build-filename "weekly-report" start end))))
 
 (defvar sbw/org-review-sprint-end-date
-  (sbw/time-from-org-string "2017-06-02")
+  (sbw/time-from-org-string "2017-11-24")
   "The sprint end date.")
 
 (defvar sbw/org-review-sprint-duration-in-days
