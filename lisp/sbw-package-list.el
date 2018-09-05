7b8630056ebab8c6f8cb90f6dd42ec676149dc2a
Stu W <stubillwhite@gmail.com>
2018-09-05 14:08:41 +0100

    Switch to Dante for Haskell

--- lisp/sbw-package-list.el
+++ lisp/sbw-package-list.el
@@ -40,13 +40,14 @@
                          (:name yasnippet) ;; Yet another snippet extension for Emacs
 
                          ;; Interface
-                         (:name diminish)           ;; Abbreviate minor mode indicators
-                         (:name projectile)         ;; Project interaction library for Emacs
-                         (:name powerline)          ;; Emacs version of the Vim powerline
-                         (:name expand-region)      ;; Expand region by semantic units
-                         (:name hydra)              ;; Make Emacs bindings that stick around
-			 (:name which-key)          ;; Display available key bindings
-			 (:name undo-tree)          ;; Visualise the undo tree
+                         (:name diminish)            ;; Abbreviate minor mode indicators
+                         (:name projectile)          ;; Project interaction library for Emacs
+                         (:name powerline)           ;; Emacs version of the Vim powerline
+                         (:name expand-region)       ;; Expand region by semantic units
+                         (:name evil-nerd-commenter) ;; Efficient language-independent commenting
+                         (:name hydra)               ;; Make Emacs bindings that stick around
+			 (:name which-key)           ;; Display available key bindings
+			 (:name undo-tree)           ;; Visualise the undo tree
 
                          ;; Parenthesis management
                          (:name smartparens)  ;; Improved paredit
@@ -84,10 +85,15 @@
                          ;; ;; Elm
                          ;; elm-mode     ;; Elm mode for emacs
                          ;; flycheck-elm ;; Flycheck for Elm
-     
+
+                         ;; TODO: Implement disable
+                         ;; http://haroldcarr.com/posts/2017-10-24-emacs-haskell-dev-env.html
+                         ;; https://www.fosskers.ca/blog/nix-en.html
                          ;; Haskell
                          (:name haskell-mode) ;; Haskell mode
-                         (:name intero)       ;; Interactive Haskell development
+                         ;; (:name intero)       ;; Interactive Haskell development
+                         (:name dante)        ;; Emacs mode for interactive Haskell
+                         ;; TODO: Look into haskell-mode-stylish-buffer to auto-reformat
 
                          ;; ;; ;; JSON
                          ;; ;; json-mode ;; Major mode for editing JSON files
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-Emacs configuration.
+# Emacs configuration
+
+Personal Emacs configuration, probably not very useful to others.
+
+## Crib notes for things I always forget
+
+### Haskell mode
+
+#### Cabal projects
+
+- Enable Dante instead of Intero
+- Edit a Haskell file
+
+    -- >>> filter even (1 :. 2 :. 3 :. 4 :. 5 :. Nil)
+    -- [2,4]
+
+-- Evaluate block `C-c "`
+-- Type at point `C-c .`
+-- Info at point `C-c ,`
+
+[Dev Env](http://haroldcarr.com/posts/2017-10-24-emacs-haskell-dev-env.html)
+[Stuff](https://www.fosskers.ca/blog/nix-en.html)
+
+
+#### Stack projects
