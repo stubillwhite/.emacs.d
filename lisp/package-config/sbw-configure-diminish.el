8fa1f8d3aeb366fa605fb8dc65a70d23021d2656
Stu W <stubillwhite@gmail.com>
2019-05-03 20:49:18 +0100

    Add mw-thesaurus mode

--- lisp/package-config/sbw-configure-diminish.el
+++ lisp/package-config/sbw-configure-diminish.el
@@ -1,6 +1,7 @@
 (use-package diminish
   :config
   (progn
-    (eval-after-load "org-indent" '(diminish 'org-indent-mode))))
+    (eval-after-load "org-indent" '(diminish 'org-indent-mode))
+    (eval-after-load "mw-thesaurus" '(diminish 'mw-thesaurus-mode))))
 
 (provide 'sbw-configure-diminish)
---
+++ lisp/package-config/sbw-configure-mw-thesaurus.el
@@ -0,0 +1,20 @@
+(use-package mw-thesaurus
+  :init
+  (progn
+    (add-hook 'markdown-mode-hook 'mw-thesaurus-mode))
+
+  :config
+  (progn
+    (advice-add 'mw-thesaurus--lookup-at-point :around #'sbw/cosmetics-ensure-vertical-split-advice)
+
+    (define-key mw-thesaurus-mode-map (kbd "q") nil)
+    
+    (defun sbw/open-thesaurus ()
+      (interactive)
+      (let ((curr-window (selected-window)))
+        (mw-thesaurus--lookup-at-point)
+        (sbw/open-and-switch-to-window mw-thesaurus-buffer-name)
+        (select-window curr-window))))
+
+  :bind
+  ("<f5>" . sbw/open-thesaurus))
