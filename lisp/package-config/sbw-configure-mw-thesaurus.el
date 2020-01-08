49b480da58d1e3f560a103807df581eedcd74649
Stu W <stubillwhite@gmail.com>
2020-01-08 23:16:36 +0000

    Bind window movement keys in thesaurus mode

--- lisp/package-config/sbw-configure-mw-thesaurus.el
+++ lisp/package-config/sbw-configure-mw-thesaurus.el
@@ -8,6 +8,10 @@
     (advice-add 'mw-thesaurus-lookup-at-point :around #'sbw/cosmetics-ensure-vertical-split-advice)
 
     (define-key mw-thesaurus-mode-map (kbd "q") nil)
+    (define-key mw-thesaurus-mode-map (kbd "<S-left>") 'windmove-left)
+    (define-key mw-thesaurus-mode-map (kbd "<S-right>") 'windmove-right)
+    (define-key mw-thesaurus-mode-map (kbd "<S-up>") 'windmove-up)
+    (define-key mw-thesaurus-mode-map (kbd "<S-down>") 'windmove-down)
     
     (defun sbw/open-thesaurus ()
       (interactive)
