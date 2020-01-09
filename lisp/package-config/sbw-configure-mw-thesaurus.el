bfd2aa3a738a1d429d855533efd666f081c327ea
Stu W <stubillwhite@gmail.com>
2020-01-09 22:06:39 +0000

    Bind q to close thesaurus window

--- lisp/package-config/sbw-configure-mw-thesaurus.el
+++ lisp/package-config/sbw-configure-mw-thesaurus.el
@@ -7,12 +7,12 @@
   (progn
     (advice-add 'mw-thesaurus-lookup-at-point :around #'sbw/cosmetics-ensure-vertical-split-advice)
 
-    (define-key mw-thesaurus-mode-map (kbd "q") nil)
+    (define-key mw-thesaurus-mode-map (kbd "q") 'delete-window)
     (define-key mw-thesaurus-mode-map (kbd "<S-left>") 'windmove-left)
     (define-key mw-thesaurus-mode-map (kbd "<S-right>") 'windmove-right)
     (define-key mw-thesaurus-mode-map (kbd "<S-up>") 'windmove-up)
     (define-key mw-thesaurus-mode-map (kbd "<S-down>") 'windmove-down)
-    
+
     (defun sbw/open-thesaurus ()
       (interactive)
       (let ((curr-window (selected-window)))
