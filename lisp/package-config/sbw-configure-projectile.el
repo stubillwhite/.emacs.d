e4cab895e0755f2bb6432c3f81d7267946aac012
Stu W <stubillwhite@gmail.com>
2022-10-22 09:28:03 +0100

    Respect .projectile by using hybrid indexing

--- lisp/package-config/sbw-configure-projectile.el
+++ lisp/package-config/sbw-configure-projectile.el
@@ -3,12 +3,16 @@
   
   :init
   (progn
-    (projectile-mode +1)
+    (projectile-mode t)
     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
     ;; Ensure that Windows is picking up the right version of grep
     (when (sbw/is-windows?)
-      (setq exec-path (append '("/usr/bin") exec-path))))
+      (setq exec-path (append '("/usr/bin") exec-path)))
+
+    (setq
+     projectile-indexing-method 'hybrid  ;; Slower processing, but respect .projectile
+     ))
 
   :config
   (progn))
